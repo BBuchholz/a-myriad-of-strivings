3b90d5bd5f1bd816c40d7c3244484baa2b414500
ENTOMDT <entomdt@gmail.com>
2022-12-23 10:38:40 -0600

    Change page title

--- src/App.svelte
+++ src/App.svelte
@@ -27,6 +27,7 @@
 </script>
 
 <svelte:head>
+  <title>A Myriad Of Services</title>
   <!-- <link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css"> -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/slate/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
